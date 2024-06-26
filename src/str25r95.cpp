2f49fa4aa444fe8e73745f6c0b461cb19851ed04
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-26 14:47:49 +0200

    add src and header file

---
+++ src/str25r95.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2024, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#include "st25r95.h"
+
+namespace sixtron {
+
+st25r95::st25r95()
+{
+}
+
+} // namespace sixtron
---
+++ st25r95/st25r95.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2024, CATIE
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef CATIE_SIXTRON_ST25R95_H_
+#define CATIE_SIXTRON_ST25R95_H_
+
+namespace sixtron {
+
+class st25r95 {
+public:
+    st25r95();
+};
+
+} // namespace sixtron
+
+#endif // CATIE_SIXTRON_ST25R95_H_
