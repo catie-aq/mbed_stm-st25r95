c625aa3e3577c6babda329deff97163beb5b0d35
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-27 14:28:31 +0200

    Add IDN function and All essential function

--- src/st25r95.cpp
+++ src/st25r95.cpp
@@ -5,9 +5,119 @@
 #include "st25r95.h"
 #include "mbed.h"
 
-ST25R95::ST25R95(SPI *spi, DigitalOut *chip_select, DigitalOut *interrupt_input)
+ST25R95::ST25R95(SPI *spi, DigitalOut *chip_select, DigitalOut *interrupt_input, int frequency)
 {
     _spi_address = spi;
     _chip_select = chip_select;
     _interrupt_input = interrupt_input;
+}
+
+void ST25R95::initialize()
+{
+    _spi_address->format(8, 0);
+    _spi_address->frequency(1000000);
+    irqn_reset();
+    _chip_select->write(0);
+    _spi_address->write(0x01);
+    ThisThread::sleep_for(1ms);
+    _chip_select->write(1);
+    irqn_reset();
+    calibrate();
+}
+
+void ST25R95::irqn_reset()
+{
+    _interrupt_input->write(1);
+    ThisThread::sleep_for(1ms);
+    _interrupt_input->write(0);
+    ThisThread::sleep_for(1ms);
+    _interrupt_input->write(1);
+    ThisThread::sleep_for(20ms);
+}
+
+void ST25R95::calibrate()
+{
+    _chip_select->write(0);
+    _spi_address->write(0x07);
+    _spi_address->write(0x0E);
+    _spi_address->write(0x03);
+    _spi_address->write(0xA1);
+    _spi_address->write(0x00);
+    _spi_address->write(0xF8);
+    _spi_address->write(0x01);
+    _spi_address->write(0x18);
+    _spi_address->write(0x00);
+    _spi_address->write(0x20);
+    _spi_address->write(0x60);
+    _spi_address->write(0x60);
+    _spi_address->write(0x00);
+    _spi_address->write(0x00);
+    _spi_address->write(0x3F);
+    _spi_address->write(0x00);
+    _chip_select->write(1);
+    _chip_select->write(0);
+    _spi_address->write(0x02);
+    _spi_address->write(0x00);
+    _spi_address->write(0x00);
+    _chip_select->write(1);
+}
+
+bool ST25R95::poll(bool default_value, uint8_t flags, uint8_t presc, uint8_t timer)
+{
+    uint8_t flag;
+    if (default_value) {
+        do {
+            _chip_select->write(0);
+            _spi_address->write(0x03);
+            flag = _spi_address->write(0x00);
+            ThisThread::sleep_for(15ms);
+            _chip_select->write(1);
+        } while ((flag & 0x08) == 0);
+        return true;
+    } else {
+        do {
+            _chip_select->write(0);
+            _spi_address->write(0x03);
+            _spi_address->write(0x03);
+            _spi_address->write(flags);
+            _spi_address->write(presc);
+            _spi_address->write(timer);
+            _spi_address->write(0x00);
+            _spi_address->write(0x00);
+            flag = _spi_address->write(0x00);
+            ThisThread::sleep_for(15ms);
+            _chip_select->write(1);
+        } while ((flag & 0x01) == 0);
+        return true;
+    }
+    return false;
+}
+
+uint8_t ST25R95::read_response()
+{
+    _chip_select->write(0);
+    _spi_address->write(0x02);
+    response_code = _spi_address->write(0x00);
+    response_length = _spi_address->write(0x00);
+    for (int i = 0; i < response_length; i++) {
+        response[i] = _spi_address->write(0x00);
+    }
+    _chip_select->write(1);
+    return response_length;
+}
+
+void ST25R95::get_idn_value()
+{
+    _chip_select->write(0);
+    _spi_address->write(0x00);
+    _spi_address->write(0x01);
+    _spi_address->write(0x00);
+    _chip_select->write(1);
+
+    poll();
+    read_response();
+    for (size_t i = 0; i < response_length; i++) {
+        printf("%02X ", response[i]);
+    }
+    printf("\n");
 }
--- st25r95/st25r95.h
+++ st25r95/st25r95.h
@@ -8,12 +8,27 @@
 
 class ST25R95 {
 public:
-    ST25R95(SPI *spi, DigitalOut *chip_select, DigitalOut *interrupt_input);
-    // ST25R95(SPI *spi, DigitalOut *chip_select, DigitalOut *interrupt_input);
+    ST25R95(SPI *spi,
+            DigitalOut *chip_select,
+            DigitalOut *interrupt_input,
+            int frequency = 1000000);
+    void initialize();
+
+    bool poll(bool default_value = true, uint8_t flags = 0, uint8_t presc = 0, uint8_t timer = 0);
+    uint8_t read_response();
+    void get_idn_value();
+
 private:
+    uint8_t response_code;
     SPI *_spi_address;
     DigitalOut *_chip_select;
     DigitalOut *_interrupt_input;
+    uint8_t _protocol_select;
+    uint32_t tag_value_32;
+    void irqn_reset();
+    void calibrate();
+    uint8_t response[512];
+    uint8_t response_length;
 };
 
 #endif // CATIE_SIXTRON_ST25R95_H_
