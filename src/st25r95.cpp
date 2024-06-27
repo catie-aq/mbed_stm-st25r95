294461be3f97853335b4dc2c4ab73ba28ef716b0
Gairaud Alexis <a.gairaud@catie.fr>
2024-06-27 15:52:40 +0200

    Add all function for Protocol 14443-A

--- src/st25r95.cpp
+++ src/st25r95.cpp
@@ -10,12 +10,12 @@ ST25R95::ST25R95(SPI *spi, DigitalOut *chip_select, DigitalOut *interrupt_input,
     _spi_address = spi;
     _chip_select = chip_select;
     _interrupt_input = interrupt_input;
+    _spi_address->format(8, 0);
+    _spi_address->frequency(frequency);
 }
 
 void ST25R95::initialize()
 {
-    _spi_address->format(8, 0);
-    _spi_address->frequency(1000000);
     irqn_reset();
     _chip_select->write(0);
     _spi_address->write(0x01);
@@ -120,4 +120,80 @@ void ST25R95::get_idn_value()
         printf("%02X ", response[i]);
     }
     printf("\n");
+}
+
+void ST25R95::select_14443_A_protocol(
+        bool default_protocol, uint8_t data_rate, uint8_t pp, uint8_t mm, uint8_t dd)
+{
+    if (default_protocol) {
+        _chip_select->write(0);
+        _spi_address->write(0x00); // Prepare to send command
+        _spi_address->write(0x02); // Send command
+        _spi_address->write(0x02); // Length of Data
+        _spi_address->write(0x02); // Data (Protocol ID)
+        _spi_address->write(0x00); // Data Rate
+        _chip_select->write(1);
+    } else {
+        _chip_select->write(0);
+        _spi_address->write(0x00); // Prepare to send command
+        _spi_address->write(0x02); // Send command
+        _spi_address->write(0x05); // Length of Data
+        _spi_address->write(0x02); // Data (Protocol ID)
+        _spi_address->write(data_rate); // Data Rate
+        // PP MM DD Value are useful to calculate FDT Frame delay time ref 5.4 p15 of st25r95
+        // datasheet
+        _spi_address->write(pp); // PP
+        _spi_address->write(mm); // MM
+        _spi_address->write(dd); // DD
+        _chip_select->write(1);
+    }
+
+    _protocol_select = 0x02;
+}
+
+char *ST25R95::get_tag_value()
+{
+    send_receive_command();
+    if (read_response()) {
+        tag_value_32 = ((uint32_t)response[0] << 24) | ((uint32_t)response[1] << 16)
+                | ((uint32_t)response[2] << 8) | ((uint32_t)response[3] << 0);
+        char tag_value[response_length];
+        for (size_t i = 0; i < response_length; i++) {
+            tag_value[i] = response[i];
+            printf("%02X ",tag_value[i]);
+        }
+        printf("\n");
+        return tag_value;
+    } else {
+        return 0;
+    }
+}
+
+void ST25R95::send_receive_command()
+{
+    switch (_protocol_select) {
+        case 0x02:
+            _chip_select->write(0);
+            _spi_address->write(0x00);
+            _spi_address->write(0x04); // Command
+            _spi_address->write(0x02); // Length
+            _spi_address->write(0x26); // Data
+            _spi_address->write(0x07); // Data
+            _chip_select->write(1);
+            poll();
+            read_response();
+            _chip_select->write(0);
+            _spi_address->write(0x00);
+            _spi_address->write(0x04); // Command
+            _spi_address->write(0x03); // Length
+            _spi_address->write(0x93); // Data
+            _spi_address->write(0x20); // Data
+            _spi_address->write(0x08); // Data
+            _chip_select->write(1);
+            poll();
+            break;
+
+        default:
+            break;
+    }
 }
--- st25r95/st25r95.h
+++ st25r95/st25r95.h
@@ -13,12 +13,18 @@ public:
             DigitalOut *interrupt_input,
             int frequency = 1000000);
     void initialize();
-
     bool poll(bool default_value = true, uint8_t flags = 0, uint8_t presc = 0, uint8_t timer = 0);
     uint8_t read_response();
     void get_idn_value();
+    void select_14443_A_protocol(bool default_protocol = true,
+            uint8_t data_rate = 0,
+            uint8_t pp = 0,
+            uint8_t mm = 0,
+            uint8_t dd = 0);
+    char *get_tag_value();
 
 private:
+    void send_receive_command();
     uint8_t response_code;
     SPI *_spi_address;
     DigitalOut *_chip_select;
